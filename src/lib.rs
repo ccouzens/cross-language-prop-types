2384182ededd4de07088691e63ff768efe0cb5eb
Chris Couzens <ccouzens@gmail.com>
2025-02-13 19:18:09 +0000

    Begin adding error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::default;
-
 use indexmap::IndexMap;
 use pest::Parser;
 use pest_derive::Parser;
 
+use thiserror::Error;
 #[derive(Parser)]
 #[grammar = "propTypes.pest"]
 pub struct PestParser;
@@ -19,9 +18,20 @@ struct CrossCompiler<'a> {
     composite_types: IndexMap<&'a str, CompositeType<'a>>,
 }
 
+#[derive(Error, Debug, PartialEq, Eq)]
+enum CrossCompilerParseError {
+    #[error("input invalid to Pest")]
+    PestParse(#[from] Box<pest::error::Error<Rule>>),
+    #[error("field {field_name:?} name is duplicated in struct {struct_name:?}")]
+    NonUniqueStructField {
+        field_name: String,
+        struct_name: String,
+    },
+}
+
 impl<'a> CrossCompiler<'a> {
-    fn parse(input: &'a str) -> Self {
-        let pest_composite_declarations = PestParser::parse(Rule::file, input).unwrap();
+    fn parse(input: &'a str) -> Result<Self, CrossCompilerParseError> {
+        let pest_composite_declarations = PestParser::parse(Rule::file, input).map_err(Box::new)?;
         let mut output = Self {
             composite_types: Default::default(),
         };
@@ -39,7 +49,7 @@ impl<'a> CrossCompiler<'a> {
                 | Rule::WHITESPACE => unreachable!(),
                 Rule::structDeclaration => {
                     let mut tokens = pest_composite_declaration.into_inner();
-                    let type_name = tokens.next().unwrap().as_str();
+                    let composite_type_name = tokens.next().unwrap().as_str();
                     let mut struct_fields = IndexMap::new();
                     for pest_struct_field in tokens {
                         match pest_struct_field.as_rule() {
@@ -48,7 +58,10 @@ impl<'a> CrossCompiler<'a> {
                                 let field_name = tokens.next().unwrap().as_str();
                                 let type_name = tokens.next().unwrap().as_str();
                                 if struct_fields.insert(field_name, type_name).is_some() {
-                                    panic!("expected field name to be unique");
+                                    return Err(CrossCompilerParseError::NonUniqueStructField {
+                                        field_name: field_name.to_string(),
+                                        struct_name: composite_type_name.to_string(),
+                                    });
                                 }
                             }
                             _ => unreachable!(),
@@ -57,7 +70,7 @@ impl<'a> CrossCompiler<'a> {
                     if output
                         .composite_types
                         .insert(
-                            type_name,
+                            composite_type_name,
                             CompositeType::Struct {
                                 fields: struct_fields,
                             },
@@ -69,13 +82,33 @@ impl<'a> CrossCompiler<'a> {
                 }
             }
         }
-        output
+        Ok(output)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_errors_with_bad_syntax() {
+        let input = "struct Person [ birthYear: u32, name: string, ]";
+        let c = CrossCompiler::parse(input);
+        assert!(matches!(c, Err(CrossCompilerParseError::PestParse(_))));
+    }
+
+    #[test]
+    fn it_errors_with_duplicated_field() {
+        let input = "struct Person { birthYear: u32, birthYear: string, }";
+        let c = CrossCompiler::parse(input);
+        assert_eq!(
+            c.err(),
+            Some(CrossCompilerParseError::NonUniqueStructField {
+                field_name: "birthYear".to_string(),
+                struct_name: "Person".to_string()
+            })
+        );
+    }
 
     #[test]
     fn it_works() {
