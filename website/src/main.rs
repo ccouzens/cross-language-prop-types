cdccee46292bc08fb496186d198eb60d87ade83f
Chris Couzens <ccouzens@gmail.com>
2025-03-08 13:04:23 +0000

    Begin setting up the demo website

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,3 +1,17 @@
+use leptos::{ev, html::*, prelude::*};
+
+const INITIAL_VALUE: &'static str = include_str!("./initial-value.clpt");
+
+pub fn app() -> impl IntoView {
+    let (input, set_input) = signal(INITIAL_VALUE.to_string());
+    div().child(
+        (textarea()
+            .child(input.get_untracked())
+            .prop("value", move || input.get())
+            .on_target(ev::input, move |ev| set_input.set(ev.target().value()))),
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    leptos::mount::mount_to_body(|| app())
 }
