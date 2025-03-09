c5dde1454dd51657298095c7024573533074f08b
Chris Couzens <ccouzens@gmail.com>
2025-03-09 11:22:30 +0000

    Bring in results of parsing the input
    I struggled a bit with the architecture, where I wanted to put a struct
    in a signal that depended on the lifetime of a different signal.
    
    This obviously wasn't going to work, but after discussing it with a
    special someone, she pointed out that as I was displaying as a string,
    I didn't actually need the struct in the signal.

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,15 +1,21 @@
+use cross_language_prop_types::CrossCompiler;
 use leptos::{ev, html::*, prelude::*};
 
 const INITIAL_VALUE: &'static str = include_str!("./initial-value.clpt");
 
 pub fn app() -> impl IntoView {
     let (input, set_input) = signal(INITIAL_VALUE.to_string());
-    div().child(
-        (textarea()
+    let parsed =
+        Memo::new(move |_| format!("{:?}", CrossCompiler::parse_and_validate(&input.get())));
+    div().child((
+        h2().child("Input"),
+        textarea()
             .child(input.get_untracked())
             .prop("value", move || input.get())
-            .on_target(ev::input, move |ev| set_input.set(ev.target().value()))),
-    )
+            .on_target(ev::input, move |ev| set_input.set(ev.target().value())),
+        h2().child("Status"),
+        pre().child(parsed),
+    ))
 }
 
 fn main() {
